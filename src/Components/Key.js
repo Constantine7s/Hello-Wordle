1d62e905417b5f6ff3e8128f553d617cc872a16b
Konstantin Shambazov <k.shambazov@gmail.com>
2022-07-27 22:10:07 +0900

    feat: add enter and delete key functionality

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -2,13 +2,25 @@ import React, { useContext } from 'react';
 import { MyContext } from '../App';
 
 function Key({ val, big }) {
-  const { guesses, setGuesses, currGuess, setCurrGuess } = useContext(MyContext);
+  const { guesses, setGuesses, currGuess, setCurrGuess } =
+    useContext(MyContext);
 
   const chooseLetter = () => {
     const board = [...guesses];
-    board[currGuess.guess][currGuess.letterPos] = val;
-    setGuesses(board);
-    setCurrGuess({...currGuess, letterPos: currGuess.letterPos + 1 });
+    if (val === 'ENTER') {
+      if (currGuess.letterPos !== 5) return;
+      setCurrGuess({ guess: currGuess.guess + 1, letterPos: 0 });
+    } else if (val === 'DELETE') {
+      if (currGuess.letterPos === 0) return;
+      board[currGuess.guess][currGuess.letterPos - 1] = '';
+      setGuesses(board);
+      setCurrGuess({ ...currGuess, letterPos: currGuess.letterPos - 1 });
+    } else {
+      if (currGuess.letterPos > 4) return;
+      board[currGuess.guess][currGuess.letterPos] = val;
+      setGuesses(board);
+      setCurrGuess({ ...currGuess, letterPos: currGuess.letterPos + 1 });
+    }
   };
   return (
     <div className="key" id={big && 'big'} onClick={chooseLetter}>
