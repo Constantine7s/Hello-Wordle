5aac530148095012cb26078f4f091605b2b1e541
Konstantin Shambazov <k.shambazov@gmail.com>
2022-07-27 21:49:44 +0900

    feat: add select letter on click functionality

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Keyboard from './Components/Keyboard';
 import { createContext, useState } from 'react';
 import { defaultGuesses } from './Words';
 
-
 export const MyContext = createContext();
 
 function App() {
-  const [guesses, setGuesses] = useState(defaultGuesses)
+  const [guesses, setGuesses] = useState(defaultGuesses);
+  const [currGuess, setCurrGuess] = useState({guess: 0, letterPos: 0});
+
   return (
     <div className="App">
       <nav>
         <h1>Hello Wordle</h1>
       </nav>
-      <MyContext.Provider value={{guesses, setGuesses}}>
-        <Guesses />
-        <Keyboard />
+      <MyContext.Provider value={{ guesses, setGuesses, currGuess, setCurrGuess}}>
+        <section className="game">
+          <Guesses />
+          <Keyboard />
+        </section>
       </MyContext.Provider>
     </div>
   );
--- src/Components/Key.js
+++ src/Components/Key.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MyContext } from '../App';
 
-function Key({val, big}) {
-  return <div className="key" id={big && "big"}>{val}</div>;
+function Key({ val, big }) {
+  const { guesses, setGuesses, currGuess, setCurrGuess } = useContext(MyContext);
+
+  const chooseLetter = () => {
+    const board = [...guesses];
+    board[currGuess.guess][currGuess.letterPos] = val;
+    setGuesses(board);
+    setCurrGuess({...currGuess, letterPos: currGuess.letterPos + 1 });
+  };
+  return (
+    <div className="key" id={big && 'big'} onClick={chooseLetter}>
+      {val}
+    </div>
+  );
 }
 
 export default Key;
